0cc6cc7d85860154d456551cf62c3de9ad6a372f
Luteces <ebompuis.dev@gmail.com>
2020-03-26 12:06:24 +0100

    rename package

--- src/boat/Fleet.java
+++ src/boat/Fleet.java
@@ -7,7 +7,7 @@ public class Fleet {
     private List<Boat> fleet = new ArrayList<>();
 
     public String addBoat(int x, int y, int model, String direction){
-        String resultat = "";
+        String result = "";
 
         if (fleet.size() < 5){ // Si la flotte n'est pas pleine
             boolean canAdd = true; // Variable pour voir si on peux ajouter
@@ -19,20 +19,20 @@ public class Fleet {
                     }
                     if (tampModelCount == 2){ // Si il y en a déjà 2 alors on ne peux pas rajouter
                         canAdd = false;
-                        resultat = "Il existe déjà 2 bateaux de longueur 2";
+                        result = "Il existe déjà 2 bateaux de longueur 2";
                     }
                 }else{  // Si on veux ajouter un bateau d'une autre longueur
                     if (fleet.get(i).getModel() == model){   // Vérification que le bateau de cette longueur n'existe pas
                         canAdd = false;
-                        resultat = "Il existe déjà un bateau de longueur "+model;
+                        result = "Il existe déjà un bateau de longueur "+model;
                     }
                 }
             }
             if (canAdd){    // Si toutes les vérifications sont bonnes alors on ajoute
                 fleet.add(new Boat(x,y,model,direction));
-                resultat = "Le bateau a bien été ajouté";
+                result = "Le bateau a bien été ajouté";
             }
         }
-        return resultat;
+        return result;
     }
 }
